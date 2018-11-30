066e9516a4923374586a835b60b3a70627082e41
Rajesh Nayak <rajnayak2505@gmail.com>
2018-11-30 15:13:22 +0530

    views is modified to show the template

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     # return HttpResponse("<h1>The Home Page</h1>")
-    return render(request, "pages/base.html")
+    return render(request, "pages/page.html")
