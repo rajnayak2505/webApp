ca75e10d08f6dc7c883f1b1bc82d18c22100e479
Rajesh Nayak <rajnayak2505@gmail.com>
2018-11-30 15:00:09 +0530

    Home Page and model is created

---
+++ pages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    # return HttpResponse("<h1>The Home Page</h1>")
+    return render(request, "pages/base.html")
